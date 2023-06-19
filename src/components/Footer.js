1e47ec61acd6167480848c85abc353b8f104f738
Prabin Tamang <prabint1014@gmail.com>
2023-06-19 17:11:45 +0545

    completed the styling of the footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from "react";
+import {
+  FacebookOutlined,
+  Instagram,
+  Twitter,
+  ArrowForward,
+} from "@mui/icons-material";
+
 import "./Footer.css";
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
-        {/* Contact us starts from here */}
+        {/* Category section starts from here */}
+
         <div className="footer-category">
           <h3> Top Categories </h3>
           <ul>
@@ -15,6 +23,8 @@ export const Footer = () => {
             <li> Men Boots </li>
           </ul>
         </div>
+
+        {/* Contact section starts from here */}
 
         <div className="footer-contact">
           <h3> Contact Us </h3>
@@ -30,6 +40,9 @@ export const Footer = () => {
             </li>
           </ul>
         </div>
+
+        {/* Information section starts from here  */}
+
         <div className="footer-information">
           <h3> Information </h3>
           <ul>
@@ -48,22 +61,43 @@ export const Footer = () => {
             </li>
           </ul>
         </div>
+
+        {/* social section starts from here  */}
+
         <div className="footer-social">
           <h3> Let's Stay Connected </h3>
           <p> Enter your email to unlock 10% off </p>
           <div className="footer-social-email">
-            <input type="email" placeholder="Type your email here"></input>
+            <input
+              className="footer-social-email-input"
+              type="email"
+              placeholder="Type your email here"
+            ></input>
+            <div className="footer-social-submit">
+              <ArrowForward fontSize="large" sx={{ color: "white" }} />
+            </div>
           </div>
           <h3> Follow Us </h3>
           <ul>
-            <li> Facebook </li>
-            <li> Instagram </li>
-            <li> Tiktok </li>
+            <li>
+              <FacebookOutlined fontSize="large" sx={{ color: "white" }} />
+            </li>
+            <li>
+              <Instagram fontSize="large" style={{ color: "white" }} />
+            </li>
+            <li>
+              <Twitter fontSize="large" style={{ color: "white" }} />
+            </li>
           </ul>
         </div>
       </div>
+
+      {/* copyright section starts from here  */}
+
       <div className="footer-copyright">
-        <p> </p>
+        <p className="footer-copyright-text">
+          &copy; 2023 Elevate,Inc. All Rights Reserved
+        </p>
       </div>
     </footer>
   );
