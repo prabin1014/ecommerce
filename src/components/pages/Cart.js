acc971ea4683b73989f07c9c3edf7d08ada0ac14
Prabin Tamang <prabint1014@gmail.com>
2023-06-22 00:18:14 +0545

    changed cart js

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Cart = () => {
   return (
     <div>
-      <h1>This feature is in development phase. </h1>
+      <h1>This feature is in the development phase. </h1>
     </div>
   );
 };
