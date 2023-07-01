c88ba12f9f68100d6369af57642c913f232331da
Prabin Tamang <prabint1014@gmail.com>
2023-07-02 00:22:40 +0545

    tiny change in pages

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -4,6 +4,8 @@ export const Cart = () => {
   return (
     <div>
       <h1>This feature is in the development phase. </h1>
+      <h2> See all the products</h2>
+      <h3> Now...</h3>
     </div>
   );
 };
--- src/components/pages/Wish.js
+++ src/components/pages/Wish.js
@@ -5,6 +5,8 @@ export const Wish = () => {
   return (
     <div className="wishlist">
       <h1>Wishlist item goes here</h1>
+      <h2> See all the products</h2>
+      <h3> Now...</h3>
     </div>
   );
 };
