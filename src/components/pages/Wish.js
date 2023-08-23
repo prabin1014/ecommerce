640784ed4076aa137a043ae807c7070364b5da8a
Prabin Tamang <prabint1014@gmail.com>
2023-08-23 12:09:44 +0545

    change in wishlist

--- src/components/pages/Wish.js
+++ src/components/pages/Wish.js
@@ -4,7 +4,7 @@ import "./wishlist.css";
 export const Wish = () => {
   return (
     <div className="wishlist">
-      <h1>Wishlist item goes here</h1>
+      <h1>Wishlist item goes here more and more</h1>
       <h2> See all the products</h2>
       <h3> Now...</h3>
     </div>
