65c84c6cc3bb854b222b4be8ab927efbcf0c3af5
Prabin Tamang <prabint1014@gmail.com>
2023-08-27 16:16:02 +0545

    change in wishlist

--- src/components/pages/Wish.js
+++ src/components/pages/Wish.js
@@ -6,7 +6,6 @@ export const Wish = () => {
     <div className="wishlist">
       <h1>Wishlist item goes here more and more</h1>
       <h2> See all the products</h2>
-      <h3> Now...</h3>
     </div>
   );
 };
